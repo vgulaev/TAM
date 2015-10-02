cc6ffa06f5c0eee9d5d2f54ad05441a38e89124e
vgulaev <vgulaev@yandex.ru>
2015-10-02 16:24:25 +0500

    Up

--- bpi.js
+++ bpi.js
@@ -5,9 +5,12 @@ var BPIElement = function ( argument ) {
 
 function create_map( one ) {
 	var funcBody = one.toString();
-	var strelement = funcBody.split( funcBody.split( "\n" ) ); 
+	var strelement = funcBody.split( "\n" ); 
 	var elements = [];
+	for ( var i = 0; i < strelement.length; i++ ) {
+		console.log( strelement[ i ] );
+	};
 	alert( funcBody );
 }
 
-create_map( БизнесПроцесс );
+//create_map( БизнесПроцесс );
